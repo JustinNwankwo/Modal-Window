cdbcc773404dab1a68f9b19a0751483493ecf77e
Justin Nwankwo <nuhwainks@gmail.com>
2025-05-23 15:21:58 -0400

    changes

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+const button = document.querySelectorAll('.show-modal')
+const modal = document.querySelector(".modal")
+
+
+for (let i = 0; i < button.length; i++ ) {
+button[i].addEventListener("click", function(){
+modal.classList.toggle("hidden")
+})
+}
+
