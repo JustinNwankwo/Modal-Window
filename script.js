f0d0b6409c378b8a35aeea7e6d1d070cd8197651
Justin Nwankwo <nuhwainks@gmail.com>
2025-05-26 08:47:46 -0400

    changes

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
 'use strict';
 
-const button = document.querySelectorAll('.show-modal')
-const modal = document.querySelector(".modal")
+const button = document.querySelectorAll('.show-modal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const close = document.querySelector('.close-modal');
 
-
-for (let i = 0; i < button.length; i++ ) {
-button[i].addEventListener("click", function(){
-modal.classList.toggle("hidden")
-})
+for (let i = 0; i < button.length; i++) {
+  button[i].addEventListener('click', function () {
+    modal.classList.toggle('hidden');
+    overlay.classList.toggle('hidden');
+  });
 }
 
+close.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+overlay.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Escape') {
+    overlay.classList.add('hidden');
+    modal.classList.add('hidden');
+  }
+});
